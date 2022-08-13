3e665d9bb48b95873f111c4594311ce85e5eb361
Ula Aripov <ulugbeks@gmail.com>
2022-08-12 23:26:43 -0400

    Try new actions draft 9

---
+++ spec/welcome_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe WelcomeController, type: :controller do
+    describe '#package_status' do
+        context 'when package is delivered' do
+            # it should send notice to customer
+            # it should mark order as completed
+        end 
+        
+        context 'when package is pending' do
+        end
+
+        context 'when package is shipped' do
+            it 'returns the updated group' do
+            end
+            it 'returns the updated group' do
+            end
+        end
+    end
+end
